f234c5d2385b0be0a16a486722a93a14d8fcaada
billbinxx <sarindabilbina@gmail.com>
2026-04-08 16:00:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -88,47 +88,46 @@ elif menu == "Perhitungan":
 
     #INPUT DATA ALTERNATIF
     elif menu == "Input Data":
-    st.title("Input Data")
-
-    # INPUT DATA ALTERNATIF
-    st.subheader("Input Data Alternatif")
-
-    data = pd.DataFrame({
-        "Nama": ["Siswa 1", "Siswa 2"],
-        "Kelas": ["X TKJ 1", "XI TOT"],
-        "Tanggungan": [2,3],
-        "Status": [1,2],
-        "Akademik": [3,2],
-        "Penghasilan": [2,1],
-        "Motivasi": [3,3],
-    })
-
-    edited_data = st.data_editor(data, num_rows="dynamic")
-
-    # ===== MATRIX INPUT =====
-    st.subheader("Input Matriks Perbandingan Kriteria")
-
-    with st.expander("📌 Skala Saaty (1–9)"):
-        st.write("""
-        1 = Sama penting  
-        3 = Sedikit lebih penting  
-        5 = Lebih penting  
-        7 = Sangat penting  
-        9 = Mutlak  
-        """)
-
-    criteria = ["Tanggungan","Status","Akademik","Penghasilan","Motivasi"]
-
-    matrix_df = pd.DataFrame(
-        np.ones((5,5)),
-        columns=criteria,
-        index=criteria
-    )
-
-    edited_matrix = st.data_editor(matrix_df)
-
-    # SIMPAN KE SESSION
-    if st.button("Simpan Data"):
-        st.session_state.data = edited_data
-        st.session_state.matrix = edited_matrix.values
-        st.success("Data berhasil disimpan! Lanjut ke menu Hasil Perhitungan")
+        st.title("Input Data")
+    
+        st.subheader("Input Data Alternatif")
+    
+        data = pd.DataFrame({
+            "Nama": ["Siswa 1", "Siswa 2"],
+            "Kelas": ["X TKJ 1", "XI TOT"],
+            "Tanggungan": [2,3],
+            "Status": [1,2],
+            "Akademik": [3,2],
+            "Penghasilan": [2,1],
+            "Motivasi": [3,3],
+        })
+    
+        edited_data = st.data_editor(data, num_rows="dynamic")
+    
+        # ===== MATRIX INPUT =====
+        st.subheader("Input Matriks Perbandingan Kriteria")
+    
+        with st.expander("📌 Skala Saaty (1–9)"):
+            st.write("""
+            1 = Sama penting  
+            3 = Sedikit lebih penting  
+            5 = Lebih penting  
+            7 = Sangat penting  
+            9 = Mutlak  
+            """)
+    
+        criteria = ["Tanggungan","Status","Akademik","Penghasilan","Motivasi"]
+    
+        matrix_df = pd.DataFrame(
+            np.ones((5,5)),
+            columns=criteria,
+            index=criteria
+        )
+    
+        edited_matrix = st.data_editor(matrix_df)
+    
+        # SIMPAN KE SESSION
+        if st.button("Simpan Data"):
+            st.session_state.data = edited_data
+            st.session_state.matrix = edited_matrix.values
+            st.success("Data berhasil disimpan! Lanjut ke menu Hasil Perhitungan")
