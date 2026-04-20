778eb17f3b01bcec21e5c2e0233c4c0637e60e7f
billbinxx <sarindabilbina@gmail.com>
2026-04-20 21:47:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -286,7 +286,7 @@ elif menu == "Analisis Sensitivitas":
 
         for i, k in enumerate(criteria):
 
-            persen = 10
+            persen = 20
             
             st.markdown(f"### 🔄 Skenario: {k} dinaikkan {persen}%")
             
